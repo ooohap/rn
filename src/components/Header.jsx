805b6ace5df81bf0a7e13799a903eacadf4c9132
hamdyyemadd <hamdyyemad@aol.com>
2024-10-20 23:06:11 +0300

    Update Header.jsx

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -5,7 +5,7 @@ const MemoizedNavbar = React.memo(Navbar); // Wrap Navbar with React.memo
 
 export default function Header() {
   return (
-    <header id="home" className="relative w-full">
+    <header id="home" className="relative w-full mb-32">
       <div className="relative text-center text-gray-400">
         <MemoizedNavbar /> {/* Use MemoizedNavbar */}
       </div>
