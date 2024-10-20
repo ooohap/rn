a56ff096e368f6f04287af5296c8453fdd34e2a5
hamdyyemadd <hamdyyemad@aol.com>
2024-10-21 00:20:17 +0300

    chaning hero pic
    chaning hero pic

--- src/components/home/Section1.jsx
+++ src/components/home/Section1.jsx
@@ -4,7 +4,7 @@ export default function Section1() {
   return (
     <section
       id="hero"
-      className="relative h-full overflow-hidden bg-auto bg-center md:bg-center pb-20 "
+      className="relative h-full overflow-hidden bg-cover bg-center pb-20 "
       style={{
         backgroundImage: `url('/assets/home/hero-new.jpg')`, // Add a gradient overlay with opacity
 
